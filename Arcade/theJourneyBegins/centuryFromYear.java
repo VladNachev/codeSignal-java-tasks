23ca275909ccc79fba0e7a2c9f715fcd4ea0b375
Vladimir Nachev (Vlad) <76738713+VladNachev@users.noreply.github.com>
2023-01-27 00:13:38 +0200

    add centuryFromYear.java

---
+++ Arcade/theJourneyBegins/centuryFromYear.java
@@ -0,0 +1,11 @@
+int solution(int year) {
+    int century ;
+    if (year % 100 == 0) {
+        century = (year / 100);
+    }
+    else {
+        century = ((year / 100) + 1);
+        
+    }
+    return century;
+}
